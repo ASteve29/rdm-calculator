2528c07cdd66fd5bdd1ddf14a23fb6eebd58318f
Stephen <29hyperloop@gmail.com>
2026-09-01 12:29:59 +0100

    Added the possible structure for RDM numbers

--- rdm.cpp
+++ rdm.cpp
@@ -1,7 +1,16 @@
 #pragma once
 
+class Integer {
+};
+
+class RDMDecimal {
+};
+
 class RDMNumber {
     public:
         
     private:
+        Integer intValue;
+        RDMDecimal decimalValue;
+
 };
